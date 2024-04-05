7fea66b0435dd94d77fff4d3676c315b3d25115e
Osirus Jack <methanblun@gmail.com>
2024-04-05 14:40:53 +0000

    maping houseHoldItems

--- main.js
+++ main.js
@@ -1,34 +1,18 @@
-const testArray = ["moon", 123, true, ["nested", "array"], "earth", 456, false,[],1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000];
-
-function classifyElements() {
-  const arrays = [];
-  const nonArrays = [];
-
-  testArray.forEach((element) => {
-    if (Array.isArray(element)) {
-      arrays.push(element);
-    } else {
-      nonArrays.push(element);
-    }
-  });
-
-  console.log("Les éléments du tableau classés par type :");
-  console.log("Arrays:", arrays);
-  console.log("Non-Arrays:", nonArrays);
-}
-
-classifyElements();
+const householdItems = [
+  "Table",
+  "Chair",
+  "Fan",
+  "bich",
+  "tiger",
+  "hunder",
+  "huns",
+  "visigoth",
+];
 
 
 
+const a = householdItems.length
 
-// const testArray = ["moon", "star", "black houle", "meteor", "earth"];
 
-// function checkit() {
-//   if (testArray.filter((t) => t !== Array)) {
-//     console.log("true");
-//   } else {
-//     console.log("false");
-//   }
-// }
-// checkit()
+householdItems.map((item) => console.log(item))
+
