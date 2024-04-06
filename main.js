6c42b3b1d3f7f66a0ccfe2c82652dbe463c5c48a
Osirus Jack <methanblun@gmail.com>
2024-04-06 14:33:03 +0000

    for of loop

--- main.js
+++ main.js
@@ -1,33 +1,22 @@
-// const user = {
-//   name: "moustapha",
-//   age: 17,
-//   lastName : 'Anne',
-//   level: "high-level",
-//   isRich: false,
-
-// };
-
-const user = {
-  name: "Alex",
-  address: "15th Park Avenue",
-  age: 43,
-  department: {
-    name: "Sales",
-    Shift: "Morning",
-    address: {
-      city: "Bangalore",
-      street: "7th Residency Rd",
-      zip: 560001,
-    },
+const users = [
+  { 
+      'name': 'Alex',
+      'address': '15th Park Avenue',
+      'age': 43
   },
-};
-const keys = Object.keys(user);
-console.log(keys.length)
-keys.shift()
-console.log(keys.length)
+  { 
+      'name': 'Bob',
+      'address': 'Canada',
+      'age': 53
+  },
+  { 
+      'name': 'Carl',
+      'address': 'Bangalore',
+      'age': 26
+  }
+];
 
 
-
-
-
-
+for (let {name  , age} of users){
+  console.log(`${name} is ${age} years old `)
+}
