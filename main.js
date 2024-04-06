1694943a163c5ee7a50534aaa53739a18827ae9d
Osirus Jack <methanblun@gmail.com>
2024-04-06 15:27:05 +0000

    exercice doned

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
+// TODO : creer une fonction qui prend en argument deux
+// cles d'un object et afficher ces deux element
+
+// const users = {
+//   'name': 'mara',
+//   age : 18,
+//   isRich : false
+// }
+
+// function getIt({name, age}){
+//   console.log(`my is ${name} and i am ${age} years old`)
+
+// }
+// getIt(users)
+
+// TODO : creer un tableau users contenant 3 objets
+//  contenant chaqu'un nom et age puis afficher dans
+//  la console 'nom' is 'age' years old avec la boucle
+//  for of et la methode map()
+
 const users = [
-  { 
-      'name': 'Alex',
-      'address': '15th Park Avenue',
-      'age': 43
-  },
-  { 
-      'name': 'Bob',
-      'address': 'Canada',
-      'age': 53
-  },
-  { 
-      'name': 'Carl',
-      'address': 'Bangalore',
-      'age': 26
-  }
+  { nom: "moustapha", age: "18", isRich: false },
+  { nom: "abc", age: "19", isRich: false },
+  { nom: "ousmane", age: "50", isRich: true },
+  
 ];
 
-console.log(users)
+for(let user of users){
+  console.log(`my mae is ${user.nom}, and i am ${user.age} years old`)
+}
 
-const updated = [...users, users[1].name ='alix']
-console.log(updated)
+users.map((user) => console.log( `${user.nom}, and i am ${user.age} years old`))
