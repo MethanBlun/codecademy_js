4b3a8959c7ff2c05d0a7704d0fcec3c70adc7e81
Osirus Jack <methanblun@gmail.com>
2024-04-06 13:15:58 +0000

    extratcting object rested in nested object

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
-const user = {
-  name: "moustapha",
-  age: 17,
-  lastName : 'Anne',
-  level: "high-level",
-  isRich: false,
+// const user = {
+//   name: "moustapha",
+//   age: 17,
+//   lastName : 'Anne',
+//   level: "high-level",
+//   isRich: false,
 
-};
+// };
 
 
-const {name, lastName , fullName=`${name }${lastName}` } = user
-console.log(fullName)
+const user = { 
+  'name': 'Alex',
+  'address': '15th Park Avenue',
+  'age': 43,
+  'department':{
+      'name': 'Sales',
+      'Shift': 'Morning',
+      'address': {
+          'city': 'Bangalore',
+          'street': '7th Residency Rd',
+          'zip': 560001
+      }
+  }
+}
+
+
+const {department : {address : {city}} } = user
+console.log(city)
+
