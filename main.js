f8b38de0b1ec6ac33e731de4162714f87baa437c
Osirus Jack <methanblun@gmail.com>
2024-04-06 12:47:31 +0000

    destructuring object

--- main.js
+++ main.js
@@ -1,25 +1,10 @@
+const user = {
+  name: "moustapha",
+  age: 10,
+  level: "high-level",
+  isRich: false,
+};
 
 
-
-
-
-
-const householdItems = [
-  "Table",
-  "Chair",
-  "Fan",
-  "bich",
-  "tiger",
-  "hunder",
-  "huns",
-  "visigoth",
-];
-
-const [a,b,c,...d] = householdItems;
-console.log(d)
-
-// householdItems.map((item) => console.log(item))
-
-
-
-//comment attribuer directement chaque element du tableau a une lettre generer automatiquement si posssible
+const {name} = user
+console.log(name)
