fd52924b486ea2f3472946ec71de620cabac0c7d
Osirus Jack <methanblun@gmail.com>
2024-04-06 13:23:34 +0000

    Object.keys methode

--- main.js
+++ main.js
@@ -7,23 +7,19 @@
 
 // };
 
+const user = {
+  name: "Alex",
+  address: "15th Park Avenue",
+  age: 43,
+  department: {
+    name: "Sales",
+    Shift: "Morning",
+    address: {
+      city: "Bangalore",
+      street: "7th Residency Rd",
+      zip: 560001,
+    },
+  },
+};
 
-const user = { 
-  'name': 'Alex',
-  'address': '15th Park Avenue',
-  'age': 43,
-  'department':{
-      'name': 'Sales',
-      'Shift': 'Morning',
-      'address': {
-          'city': 'Bangalore',
-          'street': '7th Residency Rd',
-          'zip': 560001
-      }
-  }
-}
-
-
-const {department : {address : {city}} } = user
-console.log(city)
-
+console.log(Object.keys(user.department));
