ac6f9876612b5c96af56bc1e49b7d2941bb8b0fb
Osirus Jack <methanblun@gmail.com>
2024-04-17 16:37:17 +0000

    consvert response to json and extract data in another then

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 
 
-fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-    .then(response => console.log(response))
+fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+    .then(response => response.json())
+    .then(data => console.log(data))
     .catch(error => console.error(error))
