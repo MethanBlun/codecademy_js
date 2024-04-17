40d190953c27dca8cafdc94317e667c933aca189
Osirus Jack <methanblun@gmail.com>
2024-04-17 16:49:59 +0000

    if fetching in unexisting data

--- main.js
+++ main.js
@@ -2,6 +2,12 @@
 
 
 fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
-    .then(response => response.json())
+    .then(response => {
+        if(!response.ok){
+            
+            throw new Error('could not fetch ressources')
+        }
+     return response.json()
+    })
     .then(data => console.log(data.id))
     .catch(error => console.error(error))
