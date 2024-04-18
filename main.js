e7b0f52a772321d093cb5104f039c4daa8a3ab44
Osirus Jack <methanblun@gmail.com>
2024-04-18 13:01:40 +0000

    exo retry

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 
 
 
-fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
-    .then(response => {
-        if(!response.ok){
+
+const getUsersPokemon = ({target})=>{
+   const usersPokemon = target.value
+   console.log(usersPokemon)
+}
+const getPokemonFromApi = ()=>{
+    return console.log(getPokemonFromApi)
+}
+
+// fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+//     .then(response => {
+//         if(!response.ok){
             
-            throw new Error('could not fetch ressources')
-        }
-     return response.json()
-    })
-    .then(data => console.log(data.id))
-    .catch(error => console.error(error))
+//             throw new Error('could not fetch ressources')
+//         }
+//      return response.json()
+//     })
+//     .then(data => console.log(data.id))
+//     .catch(error => console.error(error))
