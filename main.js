3e80e2b1dfc802abf524da15adf9244fd98c81c0
Osirus Jack <methanblun@gmail.com>
2024-04-19 23:04:39 +0000

    use of promise object , resolve function , and reject for async javascript

--- main.js
+++ main.js
@@ -1,26 +1,46 @@
-let usersPokemon;
 
-const getUsersPokemon = ({ target }) => {
-  usersPokemon = target.value;
-};
-const getPokemonFromApi = () => {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${usersPokemon}`)
-    .then((resp) => {
-      if (!resp.ok) {
-        throw (
-          (new Error("POkemon enexistant"),
-          alert("your pokemon does not exist"))
-        );
-      }
-      return resp.json();
-    })
-    .then(function (data) {
-      dynImg = document.getElementById("dynamic-image");
-      dynImg.src = data.sprites.back_shiny;
-      //relier la source de l'img avec le repertoire dans l'api
-    })
-    .catch((error) => {
-      console.error(error);
-      alert("Votre Pokémon n'existe pas.");
-    });
-};
+
+
+
+let p = new Promise((resolve,reject)=>{
+let testReussi = true;
+
+if (testReussi){
+  resolve(console.log('le test est reussi') )
+}else { reject(console.log('le test fut un echec'))}
+})
+
+
+
+
+
+
+
+
+
+// let usersPokemon;
+
+// const getUsersPokemon = ({ target }) => {
+//   usersPokemon = target.value;
+// };
+// const getPokemonFromApi = () => {
+//   fetch(`https://pokeapi.co/api/v2/pokemon/${usersPokemon}`)
+//     .then((resp) => {
+//       if (!resp.ok) {
+//         throw (
+//           (new Error("POkemon enexistant"),
+//           alert("your pokemon does not exist"))
+//         );
+//       }
+//       return resp.json();
+//     })
+//     .then(function (data) {
+//       dynImg = document.getElementById("dynamic-image");
+//       dynImg.src = data.sprites.back_shiny;
+//       //relier la source de l'img avec le repertoire dans l'api
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//       alert("Votre Pokémon n'existe pas.");
+//     });
+// };
