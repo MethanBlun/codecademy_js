ae82d1af70f3cb406be336f0a136c37fb2f942bd
Osirus Jack <methanblun@gmail.com>
2024-04-19 23:20:32 +0000

    some code rewrite

--- main.js
+++ main.js
@@ -2,19 +2,55 @@
 
 
 
-let p = new Promise((resolve,reject)=>{
-let testReussi = false;
+const isPhoneStore = true;
+const isPhoneAvailable = true;
 
-if (testReussi){
-  resolve('success' )
-}else { reject('error')}
-})
+function processMessage(resolveCallback, rejectCallback) {
+  if (!isPhoneStore) {
+    rejectCallback({
+      name: 'Wrong store',
+      message: 'Sorry, this is a food store!',
+    });
+  } else if (!isPhoneAvailable) {
+    rejectCallback({
+      name: 'Out of stock',
+      message: 'Sorry, the X phone is out of stock!',
+    });
+  } else {
+    resolveCallback({
+      name: 'OK',
+      message: 'The X phone is available! How many you want to buy?',
+    });
+  }
+}
 
 
 
 
 
-p.then(message=>{ console.log(`le resole a bien ${message}`)}  ).catch(message => {console.log(`le message est une ${message}`)})
+
+
+
+
+
+
+
+// let p = new Promise((resolve, reject) => {
+//   let testReussi = false;
+
+//   if (testReussi) {
+//     resolve("success");
+//   } else {
+//     reject("error");
+//   }
+// });
+
+// p.then((message) => {
+//   console.log(`le resole a bien ${message}`);
+// }).catch((message) => {
+//   console.log(`le message est une ${message}`);
+// });
+
 
 
 
