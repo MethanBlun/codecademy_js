36c3b515c0f7beb2f4ccadd281ef364d328d2de0
Osirus Jack <methanblun@gmail.com>
2024-07-31 13:00:29 +0000

    object assign methode

--- main.js
+++ main.js
@@ -1,16 +1,10 @@
-const array1 = ["mark", "zuk", "may", "faal", "christ"];
-const array2 = ["mark", "zuk", "herk", "royal", "sisre"];
-const array3 = [];
 
-for (let i = 0; i < array1.length; i++) {
-  for (let j = 0; j < array2.length; j++) { // Initialisation de j à 0
-    console.log('j : ', j);
-    if (array1[i] === array2[j]) {
-      array3.push(array1[i]); // Ajouter l'élément correspondant à array3
-    }
-  }
-}
 
-console.log(array3); // Afficher les éléments similaires trouvés
 
-//TODO:creer creer deux array avecc des valeurs et une vide dont on ajoute les valeurs croisees
+const obj1 = {a:1,b:2,c:3,d:4}
+const obj2 = {d:5,e:'f'}
+const destination= {}
+
+
+Object.assign(destination,obj1,obj2)
+console.log(destination)
